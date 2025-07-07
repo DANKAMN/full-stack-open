6e416c433fd18ad8c8ab81659f53041051c65dc5
DANKAMN <danielnicholas1477@gmail.com>
2025-07-07 16:28:21 +0100

    final

--- Part-01/unicafe/src/App.jsx
+++ Part-01/unicafe/src/App.jsx
@@ -1,54 +1,60 @@
 import { useState } from 'react'
 
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+)
+
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+)
+
 const Statistics = ({ good, neutral, bad, total, average, positivePercentage }) => {
   return (
-    <div>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {average}</p>
-      <p>positive {positivePercentage}%</p>
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={average.toFixed(2)} />
+        <StatisticLine text="positive" value={`${positivePercentage.toFixed(2)}%`} />
+      </tbody>
+    </table>
   )
 }
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const total = good + bad + neutral
+  const total = good + neutral + bad
   const average = total === 0 ? 0 : (good - bad) / total
   const positivePercentage = total === 0 ? 0 : (good / total) * 100
-
 
   return (
     <div>
       <h1>give feedback</h1>
-
-      <button onClick={() => setGood((prev) => prev + 1)}>good</button>
-      <button onClick={() => setNeutral((prev) => prev + 1)}>neutral</button>
-      <button onClick={() => setBad((prev) => prev + 1)}>bad</button>
+      <Button handleClick={() => setGood(prev => prev + 1)} text="good" />
+      <Button handleClick={() => setNeutral(prev => prev + 1)} text="neutral" />
+      <Button handleClick={() => setBad(prev => prev + 1)} text="bad" />
 
       <h1>statistics</h1>
-      {
-        total >= 1 ? (
-          <Statistics 
-            good={good} 
-            bad={bad} 
-            neutral={neutral} 
-            total={total} 
-            average={average} 
-            positivePercentage={positivePercentage}  
-          />
-        ) : (
-          <p>No feedback given</p>
-        )
-      }
-      
-      
+      {total > 0 ? (
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={total}
+          average={average}
+          positivePercentage={positivePercentage}
+        />
+      ) : (
+        <p>No feedback given</p>
+      )}
     </div>
   )
 }
